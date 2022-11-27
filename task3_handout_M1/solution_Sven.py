ac5f1d5e9466940cd743edf34fb2b99053aba6ef
Sven <sven.gutjahr@yahoo.de>
2022-11-27 15:10:56 +0100

    update

--- task3_handout_M1/solution_Sven.py
+++ task3_handout_M1/solution_Sven.py
@@ -5,14 +5,16 @@ from sklearn.gaussian_process import GaussianProcessRegressor
 
 domain = np.array([[0, 5]])
 SAFETY_THRESHOLD = 1.2
+CONST_OFFSET = 1.5
 SEED = 0
 
 
 """ Solution """
 class BO_algo():
     def __init__(self):
-        f_kernel = 0.5*Matern(length_scale=0.5, nu=2.5,length_scale_bounds="fixed")
-        v_kernel = np.sqrt(2)*Matern(length_scale=0.5, nu=2.5,length_scale_bounds="fixed")
+        kernel = Matern(length_scale=0.5, nu=2.5,length_scale_bounds="fixed")
+        f_kernel = 0.5*kernel
+        v_kernel = np.sqrt(2)*kernel
         """Initializes the algorithm with a parameter configuration. """
         self.x_old = []
         self.f_old = []
@@ -20,7 +22,9 @@ class BO_algo():
         self.f_sol = []
         self.x_sol = []
 
-        self.beta = 0.01
+        self.step = 0
+        self.beta = 0.5
+
         self.gp_f = GaussianProcessRegressor(kernel=f_kernel,alpha=0.15**2)
         self.gp_v = GaussianProcessRegressor(kernel=v_kernel,alpha=0.0001**2)
 
@@ -34,6 +38,7 @@ class BO_algo():
         recommendation: np.ndarray
             1 x domain.shape[0] array containing the next point to evaluate
         """
+        #self.step += 2
         recommendation = self.optimize_acquisition_function()
         return recommendation
 
@@ -56,10 +61,8 @@ class BO_algo():
 
         # Restarts the optimization 20 times and pick best solution
         for _ in range(20):
-            x0 = domain[:, 0] + (domain[:, 1] - domain[:, 0]) * \
-                 np.random.rand(domain.shape[0])
-            result = fmin_l_bfgs_b(objective, x0=x0, bounds=domain,
-                                   approx_grad=True)
+            x0 = domain[:, 0] + (domain[:, 1] - domain[:, 0]) * np.random.rand(domain.shape[0])
+            result = fmin_l_bfgs_b(objective, x0=x0, bounds=domain, approx_grad=True)
             x_values.append(np.clip(result[0], *domain[0]))
             f_values.append(-result[1])
 
@@ -81,16 +84,30 @@ class BO_algo():
         af_value: float
             Value of the acquisition function at x
         """
-        assert check_in_domain(x)
         x_data = x.reshape(-1,1)
         
-        #mean_v, std_v = self.gp_v.predict(x_data, return_cov=True)
-        #if mean_v + std_v <= SAFETY_THRESHOLD:
-        #    return 0
+        mean_v, std_v = self.gp_v.predict(x_data, return_std=True)
+        mean_f, std_f = self.gp_f.predict(x_data, return_std=True)
 
-        mean_f, std_f = self.gp_f.predict(x_data, return_cov=True)
-        #print(f'mean v: {mean_v}')
-        return mean_f[0] + self.beta * std_f[0]
+        mean_v += CONST_OFFSET
+
+        # Fall ++
+        if mean_v > 1.2 and mean_v - std_v > 1.2:
+            #return mean_f[0] + self.beta/self.step * std_f[0]
+            return mean_f[0] + self.beta * std_f[0]
+
+        # Fall 0
+        if (mean_v > 1.2 and mean_v - std_v <= 1.2) or (mean_v < 1.2 and mean_v + std_v >=1.2):
+            # return mean_f[0] + self.beta/self.step * std_f[0] - 100
+            return mean_f[0] + self.beta * std_f[0] - 100
+
+        # Fall --
+        if mean_v < 1.2 and mean_v + std_v < 1.2:
+            # return mean_f[0] + self.beta/self.step * std_f[0] - 1000
+            return mean_f[0] + self.beta * std_f[0] - 1000
+
+
+        return #mean_f[0]
 
 
     def add_data_point(self, x, f, v):
@@ -107,13 +124,15 @@ class BO_algo():
             Model training speed
         """
 
+        x -= CONST_OFFSET
+
         self.x_old.append(x)
         self.f_old.append(f)
         self.v_old.append(v)
 
         x_data = np.array(self.x_old, dtype=object).reshape(-1,1)
         f_data = np.array(self.f_old, dtype=object).reshape(-1,1)
-        v_data = 1.5 - np.array(self.v_old, dtype=object).reshape(-1,1)
+        v_data = np.array(self.v_old, dtype=object).reshape(-1,1) 
         self.gp_f.fit(x_data,f_data)
         self.gp_v.fit(x_data,v_data)
 
